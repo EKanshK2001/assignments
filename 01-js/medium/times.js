0cc00092041df38cb5ad4ce1e85e2f8e9f1655bb
Ekansh <efuforofficial@gmail.com>
2023-12-09 04:19:56 +0530

    added times.js solution

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,31 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+
+    const time1 = new Date();
+    const time1ms = time1.getTime();
+    // console.log(time1ms);
+
+    let sum = 0;
+    for (let i = 1; i < n; i++) {
+        sum += i;
+    }
+
+    const time2 = new Date();
+    const time2ms = time2.getTime();
+    // console.log(time2ms);
+    
+    const diff = time2ms - time1ms;
+    return diff;
 }
+
+console.log(calculateTime(10) + 'ms');
+console.log(calculateTime(100) + 'ms');
+console.log(calculateTime(1000) + 'ms');
+console.log(calculateTime(10000) + 'ms');
+console.log(calculateTime(100000) + 'ms');
+console.log(calculateTime(1000000) + 'ms');
+console.log(calculateTime(10000000) + 'ms');
+console.log(calculateTime(100000000) + 'ms');
+console.log(calculateTime(1000000000) + 'ms');
+console.log(calculateTime(10000000000) + 'ms');
