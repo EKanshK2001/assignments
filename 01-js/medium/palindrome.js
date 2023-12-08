b61c0d92d2a0dbcd8d1dca4ad52ca8f5d424cb2e
Ekansh <efuforofficial@gmail.com>
2023-12-09 03:37:07 +0530

    added the palindrome.js solution

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,7 +4,25 @@
 */
 
 function isPalindrome(str) {
-  return true;
+  
+  if (str.length === 0 || str.length === 1) return true;
+
+  let cleanStr = str.toLowerCase().replace(/[^a-zA-Z]/g, '');
+  // console.log(cleanStr);
+
+  let left = 0, right = cleanStr.length - 1;
+  while (left < right) {
+    if (cleanStr[left] !== cleanStr[right]) {
+      return false;
+    }
+    left++; right--;
+  }
+  return true;                                                                   //soln 1  0.224s
+
+  // return cleanStr === cleanStr.split('').reverse().join('');                  //soln 2  0.219s (after removing spaces and lowercasing)
+
+  //both solutions are fine however soln 1 is better if str is actually manipulated instead of making a new cleanStr
+  //also both solutions work around a 100ms faster (313 ms vs 225ms) everytime when ran after the first attempt (? dunno why)
 }
 
 module.exports = isPalindrome;
